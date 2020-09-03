fb38ea02a885807d79a117beff773c13ff09124b
Dario Sechi <loller790@gmail.com>
2020-09-03 18:50:33 +0200

    Export not internal type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import TelegramConfiguration from './components/configuration'
 import Telegram from './components/telegram'
 import { HandlerType } from './definitions/enums'
+import { Context, Handler, HandlerMiddleware, HandlerOptions, InputFile } from './definitions/types'
 
 export default Telegram
-export { HandlerType, TelegramConfiguration }
+export { HandlerType }
+export { Context, HandlerMiddleware, HandlerOptions, Handler, InputFile }
