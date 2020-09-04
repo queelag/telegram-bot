cf0b72a5b691643ca465f5082fbf24e02fdfdf77
Dario Sechi <loller790@gmail.com>
2020-09-04 18:45:59 +0200

    Upgrade deps and handle promiselike errors

--- src/modules/tcp.ts
+++ src/modules/tcp.ts
@@ -1,7 +1,13 @@
 const tcp = <T>(f: () => Promise<T>): Promise<T | Error> => {
   return new Promise((resolve) => {
     f()
-      .then((v: T) => resolve(v))
+      .then(
+        (v: T) => resolve(v),
+        (e: any) => {
+          console.error(e)
+          resolve(new Error(e))
+        }
+      )
       .catch((e: any) => {
         console.error(e)
         resolve(new Error(e))
