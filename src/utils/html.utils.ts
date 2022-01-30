4af62498ec4abd6c3f5820879a8b5386069d932d
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-01-30 18:02:12 +0100

    Implement a progress method to render linear progress bars

--- src/utils/html.utils.ts
+++ src/utils/html.utils.ts
@@ -1,3 +1,5 @@
+import { NumberUtils } from '@queelag/core'
+
 export class HTMLUtils {
   static tags: string[] = ['b', 'strong', 'i', 'em', 'u', 'ins', 's', 'strike', 'del', 'span', 'tg-spoiler', 'a', 'code', 'pre', '/', '"']
 
@@ -6,5 +8,14 @@ export class HTMLUtils {
       .replace(new RegExp(`<(?!(${this.tags.join('|')}))`, 'gm'), '&lt;')
       .replace(new RegExp(`(?<!(${this.tags.join('|')}))>`, 'gm'), '&gt;')
       .replace(/&/gm, '&amp;')
+  }
+
+  static progress(value: number, minimum: number = 0, maximum: number = 100, size: number = 20): string {
+    let percentage: number, steps: string[]
+
+    percentage = NumberUtils.percentage(value, minimum, maximum)
+    steps = new Array(size).fill(0).map((v, k: number) => ((k * maximum) / size < percentage ? '=' : ' '))
+
+    return `<code>[${steps.join('')}]</code>`
   }
 }
